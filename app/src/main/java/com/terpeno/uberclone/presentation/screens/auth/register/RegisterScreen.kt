596c100f507f1f136be6ed05f087795a57211945
microraptor <jeloaizam@gmail.com>
2025-08-22 08:17:21 -0500

    I need to adding other files, because in the first commit this files was missed.

--- app/src/main/java/com/terpeno/uberclone/presentation/screens/auth/register/RegisterScreen.kt
+++ app/src/main/java/com/terpeno/uberclone/presentation/screens/auth/register/RegisterScreen.kt
@@ -1,19 +1,29 @@
 package com.terpeno.uberclone.presentation.screens.auth.register
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 
 @Composable
 fun RegisterScreen ( navHostController: NavHostController){
     Scaffold { paddingValues ->
-        Text(
-            modifier = Modifier.padding(paddingValues),
-            text="Entro al registor mi Papurris"
+        Box(
+            modifier= Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(Color(0xFF55EA05), Color(0xFF82CE07)),
+                    )
+                )
+                .padding(paddingValues)
         )
-
     }
 }
